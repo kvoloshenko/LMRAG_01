f1017f82c66bb5b5b6fa91fa6add001dec0c93b8
kv <kvoloshenko@gmail.com>
2023-12-03 19:00:21 +0300

    Added ollama_test_01.py

---
+++ ollama_test_01.py
@@ -0,0 +1,39 @@
+import requests
+import json
+# import gradio as gr
+
+url = "http://localhost:11434/api/generate"
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+conversation_history = []
+
+def generate_response(prompt):
+    conversation_history.append(prompt)
+
+    full_prompt = "\n".join(conversation_history)
+
+    data = {
+        "model": "saiga_mistral_7b",
+        "stream": False,
+        "prompt": full_prompt,
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        response_text = response.text
+        data = json.loads(response_text)
+        actual_response = data["response"]
+        conversation_history.append(actual_response)
+        return actual_response
+    else:
+        print("Error:", response.status_code, response.text)
+        return None
+
+if __name__ == '__main__':
+    topic ="Привет! Ты кто?"
+    answer = generate_response(topic)
+    print(f'answer={answer}')
